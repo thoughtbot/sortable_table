be6da0e80ae076432b47023707becd19487260a4
Dan Croak <dcroak@thoughtbot.com>
2009-01-02 10:33:35 -0500

    refactoring application controller into small methods

--- lib/sortable_table/app/controllers/application_controller.rb
+++ lib/sortable_table/app/controllers/application_controller.rb
@@ -2,7 +2,7 @@ module SortableTable
   module App
     module Controllers
       module ApplicationController
-  
+
         def self.included(base)
           base.class_eval do
             include InstanceMethods
@@ -15,16 +15,7 @@ module SortableTable
             mappings           = pop_hash_from_list(args)
             acceptable_columns = join_array_and_hash_keys(args, mappings)
             
-            define_method(:sort_order) do |*default| 
-              direction = params[:order] == 'ascending' ? 'asc' : 'desc'
-              column    = params[:sort] || 'created_on'
-              if params[:sort] && acceptable_columns.include?(column)
-                column = mappings[column.to_sym] || column
-                "#{column} #{direction}"
-              else
-                "#{acceptable_columns.first} #{default_sort_direction(default)}"
-              end
-            end
+            define_sort_order(acceptable_columns, mappings)
           end
           
           def pop_hash_from_list(*args)
@@ -39,15 +30,40 @@ module SortableTable
             array.collect { |each| each.to_s } + 
               hash.keys.collect { |each| each.to_s }
           end
+          
+          def define_sort_order(acceptable_columns, mappings)
+            define_method(:sort_order) do |*default| 
+              direction = params[:order] == 'ascending' ? 'asc' : 'desc'
+              column    = params[:sort] || 'created_on'
+              if params[:sort] && acceptable_columns.include?(column)
+                column = mappings[column.to_sym] || column
+                "#{column} #{direction}"
+              else
+                "#{acceptable_columns.first} #{default_sort_direction(default)}"
+              end
+            end
+          end
         end
         
         module InstanceMethods
           def default_sort_direction(default)
-            if default.any? && default.first.is_a?(Hash) && default.first.has_key?(:default)
-              default.first[:default] == 'ascending' ? 'asc' : 'desc'
+            if hash_with_default_key?(default)
+              case default.first[:default]
+              when "ascending",  "asc" then "asc"
+              when "descending", "asc" then "desc"
+              else
+                raise RuntimeError, 
+                  "valid sort orders are 'ascending' & 'descending'"
+              end
             else
-              'desc'  
+              "desc"  
             end
+          end
+          
+          def hash_with_default_key?(object)
+            object.any? && 
+              object.first.is_a?(Hash) && 
+              object.first.has_key?(:default)
           end
         end
 
